1fae9ae2d70e214f725712744145de37e0f42fbd
Brandt <brandtsheets@gmail.com>
2017-10-31 13:53:34 -0700

    Implement sub controller and basic views

--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -1,24 +1,34 @@
 class SubsController < ApplicationController
   before_action :require_login, only: [:new, :create]
   before_action :require_moderator, only: [:edit, :update]
+
   def new
+    @sub = Sub.new
   end
 
   def create
-    sub = Sub.new(sub_params)
-    sub.moderator_id = current_user.id
-    if sub.save
-      # redirect_to sub_url(sub)
+    @sub = Sub.new(sub_params)
+    @sub.moderator_id = current_user.id
+    if @sub.save
+      redirect_to sub_url(@sub)
     else
-      flash[:errors] = sub.errors.full_messages
+      flash[:errors] = @sub.errors.full_messages
       render :new
     end
   end
 
   def edit
+    @sub = Sub.find(params[:id])
   end
 
   def update
+    @sub = Sub.find(params[:id])
+    if @sub.update_attributes(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors.full_messages
+      render :edit
+    end
   end
 
   def index
@@ -26,6 +36,7 @@ class SubsController < ApplicationController
   end
 
   def show
+    @sub = Sub.find(params[:id])
   end
 
   private
@@ -35,6 +46,6 @@ class SubsController < ApplicationController
 
   def require_moderator
     sub = Sub.find(params[:id])
-    redirect_to subs_url unless sub && current_user.id == sub.moderator_id
+    redirect_to subs_url unless logged_in? && sub && current_user.id == sub.moderator_id
   end
 end
